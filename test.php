3fe5a6f474c36dd6a7d7c84ccc11cfe039baf06c
Webmaster <ilya.a@lenta.ru>
2023-06-02 17:49:54 +0300

    init;

--- test.php
+++ test.php
@@ -2,9 +2,13 @@
 
 function load_users_data($user_ids) {
     $data = [];
+    $ids = preg_replace('/\D/', ' ', $user_ids);
+    $ids = preg_replace( '/ {1,}/', ',', $ids );
+    $ids = array_filter( array_map( 'intval', explode( ',', $ids ) ) );
+    $ids = implode(',', $ids);
     $pdo = new PDO("mysql:host=localhost;dbname=database", 'root', '123123');
     $query = $pdo->prepare('SELECT * FROM users WHERE id IN(:ids)');
-    $query->bindParam(':ids',$user_ids);
+    $query->bindParam(':ids',$ids);
     $query->execute();
     while($row = $query->fetch(PDO::FETCH_OBJECT)) {
         $data[$row->id] = $row;
